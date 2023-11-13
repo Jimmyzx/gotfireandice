d1db1d2980c21933914d0c754c6a10025736123d
Jimmyzx <125935094+Jimmyzx@users.noreply.github.com>
2023-11-12 17:36:30 -0800

    Added links on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 
@@ -6,6 +7,17 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome to the Game of Thrones App</h1>
+      <ul>
+        <li>
+          <Link to="/characters">Characters</Link>
+        </li>
+        <li>
+          <Link to="/houses">Houses</Link>
+        </li>
+        <li>
+          <Link to="/charactersandhouses">Characters & Houses</Link>
+        </li>
+      </ul>
       <p>Explore the characters and houses of Westeros!</p>
     </div>
   );
