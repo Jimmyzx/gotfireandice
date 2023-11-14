9ddcaf1b25f573eb73ed16e34600c785002fc8db
Jimmyzx <125935094+Jimmyzx@users.noreply.github.com>
2023-11-13 16:42:25 -0800

    fixed img link and api issues

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,51 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './CharacterCard.css';
+
 const CharacterCard = ({ character }) => {
+  const [allegianceNames, setAllegianceNames] = useState([]);
+
+  useEffect(() => {
+    const fetchAllegianceNames = async () => {
+      try {
+        const allegianceResponses = await Promise.all(
+          character.allegiances.map((allegianceUrl) => axios.get(allegianceUrl))
+        );
+        const names = allegianceResponses.map((response) => response.data.name);
+        setAllegianceNames(names);
+      } catch (error) {
+        console.error('Error fetching allegiance data', error);
+      }
+    };
+
+    if (character.allegiances && character.allegiances.length > 0) {
+      fetchAllegianceNames();
+    }
+  }, [character.allegiances]);
+
   return (
     <div className="character-card">
-      {/* <Link to={`/characters/${character.id}`}> */}
-      <Link to={`/characters/${character.url.replace(/[^0-9]/g,"")}`} key={character.url}>
+      <Link to={`/characters/${character.url.replace(/[^0-9]/g, "")}`} key={character.url}>
         <h2>{character.name || character.aliases[0]}</h2>
-        <p>Culture: {character.culture}</p>
-        <p>Gender: {character.gender || 'Unknown'}</p>
-        <img src={character.gender ===   'Male' ? '/male-icon.png' :    '/female-icon.png'}
+        <img
+        src={process.env.PUBLIC_URL + (character.gender === 'Male' ? '/male-icon.png' : '/female-icon.png')}
         alt="Gender Icon"
         />
-        <p>Born: {character.born || 'Unknown'}</p>
-        <p>Died: {character.died || 'Unknown'}</p>
-        <p>Titles: {character.titles || 'Unknown'}</p>
-        <p>Aliases: {character.aliases || 'Unknown'}</p>
-        <p>Father: {character.father || 'Unknown'}</p>
-        <p>Mother: {character.mother || 'Unknown'}</p>
+        <p>Aliases: {character.aliases.join(', ') || 'Unknown'}</p>
+        <p>Gender: {character.gender || 'Unknown'}</p>
+        <p>Culture: {character.culture || 'Unknown'}</p>
+        <p>Titles: {character.titles.join(', ') || 'Unknown'}</p>
         <p>Spouse: {character.spouse || 'Unknown'}</p>
-        <p>Allegiances: {character.allegiances || 'Unknown'}</p>
+        <p>Allegiances: {allegianceNames.join(', ') || 'Unknown'}</p>
       </Link>
     </div>
   );
 };
+
 export default CharacterCard;
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './CharacterCard.css';
-
-// const CharacterCard = ({ character }) => {
-//   if (!character) {
-//     return "Character Not Found"; 
-//   }
-//   return (
-//     <div className="character-card">
-//       <Link to={`/characters/${character.id}`}>
-//         <h2>{character.name || character.aliases[0]}</h2>
-//         <p>Culture: {character.culture}</p>
-//         <img
-//           src={character.gender === 'Male' ? '/male-icon.png' : '/female-icon.png'}
-//           alt="Gender Icon"
-//         />
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default CharacterCard;
